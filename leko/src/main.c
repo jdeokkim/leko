edfe78433492fe3d6d4b2c5e73735b79cadddb47
jdeokkim <jdeokkim@protonmail.com>
2022-03-26 22:42:31 +0900

    Add new files

---
+++ leko/src/main.c
@@ -0,0 +1,46 @@
+/*
+    Copyright (c) 2022 jdeokkim
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "raylib.h"
+
+#define TARGET_FPS 60
+
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
+
+int main(void) {
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetTargetFPS(TARGET_FPS);
+    
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "c-krit/leko | main.c");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        
+        ClearBackground(RAYWHITE);
+
+        /* TODO: ... */
+
+        DrawFPS(8, 8);
+
+        EndDrawing();
+    }
+    
+    CloseWindow();
+
+    return 0;
+}
