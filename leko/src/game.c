c1d81178dc4c02dd4af076b470c6f575cfc3deea
jdeokkim <jdeokkim@protonmail.com>
2022-04-16 12:35:23 +0900

    Update license messages in source files

--- leko/src/game.c
+++ leko/src/game.c
@@ -1,18 +1,23 @@
 /*
     Copyright (c) 2022 jdeokkim
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <https://www.gnu.org/licenses/>.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 #include <stdlib.h>
@@ -258,6 +263,9 @@ void LoadLevel(const char *str) {
 
     // 게임 시작 시간을 설정한다.
     start_time = GetTime();
+    
+    // 게임의 현재 레벨을 설정한다.
+    current_level = (char *) str;
 
     // 게임의 진행 상황을 초기화한다.
     for (int i = 0; i < NORMAL_BLOCK_COUNT; i++)
@@ -277,19 +285,16 @@ void InitGameScreen(void) {
     ast_blocks = GetGameAsset(4);
     ast_frame = GetGameAsset(5);
 
-    ast_dragged = GetGameAsset(6);
-    ast_marked = GetGameAsset(7);
+    ast_dragged = GetGameAsset(7);
+    ast_marked = GetGameAsset(8);
 
     settings = GetGameSettings();
-
-    // TODO: ...
-    current_level = (char *) LEVEL_00;
 
     GuiSetFont(ast_font_16pt->data.font);
 
     SetGameState(GST_NORMAL);
 
-    LoadLevel(current_level);
+    LoadLevel(LEVEL_00);
 }
 
 /* 게임 플레이 화면을 업데이트한다. */
